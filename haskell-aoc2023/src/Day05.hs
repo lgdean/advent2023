72306ac7d966e66c7f6dba567c0da51f6339992b
L Dean <lgdean@users.noreply.github.com>
2023-12-05 10:42:07 -0800

    day 5, part 2, brute force

--- haskell-aoc2023/src/Day05.hs
+++ haskell-aoc2023/src/Day05.hs
@@ -1,12 +1,13 @@
 module Day05
     (
       doPart1,
---      doPart2
+      doPart2
     ) where
 
 import Lib (parseChunks)
 
 import Data.List (sort)
+import Data.List.Split (chunksOf)
 
 -- srcStart, destStart, rangeLength
 type MappingRange = (Int, Int, Int)
@@ -45,3 +46,21 @@ parseMappingRange line =
 
 parseSeedList :: String -> [Int]
 parseSeedList line = map read $ tail $ words line
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  let mappings = parseMappings input
+      seedPairs = parseSeedRanges $ head $ lines input
+      seeds = concatMap explodeRange seedPairs -- start with brute force
+      mapSeed seed = foldl processMapping seed mappings
+      destLocations = map mapSeed seeds
+  in minimum destLocations
+
+parseSeedRanges :: String -> [(Int, Int)]
+parseSeedRanges line =
+  let parts = chunksOf 2 $ tail $ words line
+      mapPair p = (read (head p), read (last p))
+  in map mapPair parts
+
+explodeRange :: (Int, Int) -> [Int]
+explodeRange (start, len) = take len [start ..]
--- haskell-aoc2023/test/Day05Spec.hs
+++ haskell-aoc2023/test/Day05Spec.hs
@@ -15,3 +15,12 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "inputs/day05"
       doPart1 input `shouldBe` 324724204
+
+  describe "Part 2" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day05-example"
+      doPart2 input `shouldBe` 46
+
+--    it "can solve Part 2" $ do
+--      input <- readFile "inputs/day05"
+--      doPart2 input `shouldBe` 104070862 -- Finished in 1936.3873 seconds
