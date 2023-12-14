7dd5387029c1bcc6597c24b1460e8c650c2fe11f
L Dean <lgdean@users.noreply.github.com>
2023-12-13 22:29:53 -0800

    code toward day 14, part 2
    found the answer with some manual steps; still want to finish the code

--- haskell-aoc2023/src/Day14.hs
+++ haskell-aoc2023/src/Day14.hs
@@ -1,10 +1,14 @@
 module Day14
     (
       doPart1,
---      doPart2
+      loadOnNorth,
+      tiltCycle,
+      doPart2
     ) where
 
 import Data.List (transpose)
+
+import Debug.Trace (trace)
 
 doPart1 :: [Char] -> Int
 doPart1 input =
@@ -20,3 +24,31 @@ tiltTowardBegin acc ('.':rest) = tiltTowardBegin (acc ++ ".") rest
 tiltTowardBegin acc ('#':rest) = acc ++ "#" ++ tiltTowardBegin [] rest
 tiltTowardBegin  _ ( _ :_rest) = error "wat"
 tiltTowardBegin acc [] = acc
+
+loadOnNorth :: [[Char]] -> Int
+loadOnNorth rows =
+  let cols = transpose rows
+      countCol col = sum $ map fst $ filter ((== 'O') . snd) $ zip [1..] $ reverse col
+  in sum $ map countCol cols
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  let rows = lines input
+      intermediateResults = iterate tiltCycle rows
+      intermediateCounts = map loadOnNorth intermediateResults
+      eventualResult = intermediateResults !! 1000000000
+  in 0
+
+tiltCycle :: [[Char]] -> [[Char]]
+tiltCycle rows =
+  let tiltedNorth = tiltAllTowardBegin $ transpose rows
+      tiltedWest = tiltAllTowardBegin $ transpose tiltedNorth
+      tiltedSouth = tiltAllTowardEnd $ transpose tiltedWest
+      tiltedEast = tiltAllTowardEnd $ transpose tiltedSouth
+  in tiltedEast
+
+tiltAllTowardBegin :: [[Char]] -> [[Char]]
+tiltAllTowardBegin = map (tiltTowardBegin [])
+
+tiltAllTowardEnd :: [[Char]] -> [[Char]]
+tiltAllTowardEnd = map (reverse . tiltTowardBegin [] . reverse)
--- haskell-aoc2023/test/Day14Spec.hs
+++ haskell-aoc2023/test/Day14Spec.hs
@@ -15,3 +15,20 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "inputs/day14"
       doPart1 input `shouldBe` 109755
+
+  describe "Part 2" $ do
+    it "can do one cycle on given example -- just checking for bugs" $ do
+      input <- readFile "inputs/day14-example"
+      expected <- readFile "inputs/day14-after-1cycle"
+      tiltCycle (lines input) `shouldBe` lines expected
+      loadOnNorth (tiltCycle (lines input)) `shouldBe` 87
+
+    it "can do three cycles on given example" $ do
+      input <- readFile "inputs/day14-example"
+      expected <- readFile "inputs/day14-after-3cycles"
+      (tiltCycle . tiltCycle . tiltCycle) (lines input) `shouldBe` lines expected
+
+    it "can handle given example" $ do
+      input <- readFile "inputs/day14-example"
+      --doPart2 input `shouldBe` 64
+      doPart2 input `shouldBe` 0
