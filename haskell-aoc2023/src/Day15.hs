2867345aed981af892f760ffeb0d7bb081d94d62
L Dean <lgdean@users.noreply.github.com>
2023-12-14 22:43:36 -0800

    day 15, part 2

--- haskell-aoc2023/src/Day15.hs
+++ haskell-aoc2023/src/Day15.hs
@@ -1,15 +1,20 @@
 module Day15
     (
       doPart1,
---      loadOnNorth,
---      tiltCycle,
---      doPart2
+      doPart2
     ) where
 
 import Data.Char (ord)
+import Data.List (deleteBy)
 import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
 
 import Lib (strip)
+
+type Lens = (String, Int)
+type Boxes = Map Int [Lens]
+data Op = Remove String | Insert Lens deriving Eq
 
 doPart1 :: [Char] -> Int
 doPart1 input =
@@ -24,3 +29,42 @@ hashFn currVal (c:rest) =
   let plusAscii = currVal + ord c
       nextVal = (plusAscii * 17) `mod` 256
   in hashFn nextVal rest
+
+doPart2 :: [Char] -> Int
+doPart2 input =
+  let str = strip input
+      parts = splitOn "," str
+      ops = map parseOp parts
+      boxes = Map.fromAscList $ zip [0..255] $ repeat [] :: Boxes
+      endState = foldl operateOn boxes ops :: Boxes
+      powers = map (uncurry focusingPower) $ Map.toAscList endState
+  in sum powers
+
+operateOn :: Boxes -> Op -> Boxes
+operateOn boxes (Remove label) =
+  Map.adjust (removeLens label) (hashFn 0 label) boxes
+operateOn boxes (Insert lens) =
+  Map.adjust (insertOrReplaceLens lens) (hashFn 0 $ fst lens) boxes
+
+removeLens :: String -> [Lens] -> [Lens]
+removeLens label = deleteBy (\(a,_) (c,_) -> a == c) (label, 0)
+
+insertOrReplaceLens :: Lens -> [Lens] -> [Lens]
+insertOrReplaceLens lens [] = [lens]
+insertOrReplaceLens lens@(newL, _) (old@(label, _):rest) =
+  if newL == label
+  then lens : rest
+  else old : insertOrReplaceLens lens rest
+
+focusingPower :: Int -> [Lens] -> Int
+focusingPower boxNum lenses =
+  (boxNum + 1) * sum (zipWith (*) [1..] (map snd lenses))
+
+parseOp :: String -> Op
+parseOp op
+  | '=' `elem` op =
+      let parts = splitOn "=" op
+      in Insert (head parts, read $ last parts)
+  | otherwise =
+      let parts = splitOn "-" op
+      in Remove (head parts)
--- haskell-aoc2023/test/Day15Spec.hs
+++ haskell-aoc2023/test/Day15Spec.hs
@@ -18,3 +18,16 @@ spec = do
     it "can solve Part 1" $ do
       input <- readFile "inputs/day15"
       doPart1 input `shouldBe` 514394
+
+  describe "Part 2" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day15-example"
+      doPart2 input `shouldBe` 145
+
+    it "can hash a particular label from an early step" $ do
+      doPart1 "hfx" `shouldBe` 6
+
+    it "can solve Part 2" $ do
+      input <- readFile "inputs/day15"
+--      doPart2 input `shouldBe` 45990 -- wrong answer from a silly parse error
+      doPart2 input `shouldBe` 236358
