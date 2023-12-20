6e4a16ef05d96c835a54a889a3795482411d9ebf
L Dean <lgdean@users.noreply.github.com>
2023-12-19 23:16:19 -0800

    day 20, first example only

---
+++ haskell-aoc2023/src/Day20.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE TupleSections #-}
+module Day20
+    (
+      doPart1,
+--      doPart2
+    ) where
+
+import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+
+import Lib (count)
+
+data Pulse = Low | High deriving (Eq, Show)
+type Message = (String, String, Pulse)
+
+data FlipFlopState = On | Off deriving (Eq, Show)
+data Module = FlipFlop FlipFlopState | Conjunction (Map String Pulse) | Broadcast deriving (Eq)
+type ModuleStates = Map String Module   -- what modules do we have?
+type ModuleConfig = Map String [String] -- and how are they hooked up?
+initFlipFlop :: Module
+initFlipFlop = FlipFlop Off
+initConjunction :: [String] -> Module
+initConjunction inputs = Conjunction (Map.fromList $ zip inputs $ repeat Low)
+
+doPart1 :: [Char] -> Int
+doPart1 input =
+  let (moduleConfig, modulePreInitStates) = parseConfig input
+      moduleInitStates = initializeConjunctions moduleConfig modulePreInitStates
+      result = pushButton moduleConfig moduleInitStates
+      totals = map (1000 *) [count Low result, count High result]
+  in product totals
+
+pushButton :: ModuleConfig -> ModuleStates -> [Pulse]
+pushButton config moduleStates =
+  let result = propagate config moduleStates [("button", "broadcaster", Low)]
+  in map (\(_, _, p) -> p) result
+
+propagate :: ModuleConfig -> ModuleStates -> [(String, String, Pulse)] -> [Message]
+propagate config modules messages =
+  let fullRound = scanl (\(acc, _) m -> propagateOne config acc m) (modules, []) messages :: [(ModuleStates, [Message])]
+      nextRound = concatMap snd fullRound :: [Message]
+      nextRoundState = fst $ last fullRound :: ModuleStates
+  in case nextRound of
+       [] -> messages
+       _ -> messages ++ propagate config nextRoundState nextRound
+
+propagateOne :: ModuleConfig -> ModuleStates -> (String, String, Pulse) -> (ModuleStates, [(String, String, Pulse)])
+propagateOne config modules _msg@(src, dest, pulse) =
+  let srcMod = modules Map.! dest
+      _result@(newState, nextP) = sendPulse (pulse, src) srcMod
+      updatedModules = Map.insert dest newState modules
+  in case nextP of
+       Nothing -> (updatedModules, [])
+       Just nP ->
+         let nextDests = config Map.! dest
+         in (updatedModules, map (dest, , nP) nextDests)
+
+sendPulse :: (Pulse, String) -> Module -> (Module, Maybe Pulse)
+sendPulse (High, _) ff@(FlipFlop _) = (ff, Nothing)
+sendPulse (Low, _) (FlipFlop Off) = (FlipFlop On, Just High)
+sendPulse (Low, _) (FlipFlop On) = (FlipFlop Off, Just Low)
+sendPulse (p, src) (Conjunction prev) =
+  let newStates = Map.insert src p prev
+      allHigh = all (== High) $ Map.elems newStates
+  in (Conjunction newStates, Just (if allHigh then Low else High))
+sendPulse (p, _) Broadcast = (Broadcast, Just p)
+
+initializeConjunctions :: ModuleConfig -> ModuleStates -> ModuleStates
+initializeConjunctions config initStates =
+  let conjunctions = Map.keys $ Map.filter (== Conjunction Map.empty) initStates
+      sources conjName = Map.keys $ Map.filter (conjName `elem`) config
+      newConjunctions = map (\name -> (name, initConjunction $ sources name)) conjunctions
+  in foldl (\acc (k,v) -> Map.insert k v acc) initStates newConjunctions
+
+parseConfig :: String -> (ModuleConfig, ModuleStates)
+parseConfig str =
+  let moduleInfos = map parseModuleLine $ lines str
+      config = Map.fromList $ map (\(name, _, dests) -> (name, dests)) moduleInfos
+      preInitStates = Map.fromList $ map (\(name, m, _) -> (name, m)) moduleInfos
+  in (config, preInitStates)
+
+parseModuleLine :: String -> (String, Module, [String])
+parseModuleLine line =
+  let parts = splitOn " -> " line
+      modulePart = head parts
+      destPart = last parts
+      (name, m) = parseModule modulePart
+  in (name, m, splitOn ", " destPart)
+
+parseModule :: String -> (String, Module)
+parseModule "broadcaster" = ("broadcaster", Broadcast)
+parseModule ('%':name)  = (name, initFlipFlop)
+parseModule ('&':name)  = (name, initConjunction []) -- oops, don't yet know inputs
+parseModule otherString = error ("cannot parse Module: " ++ otherString)
---
+++ haskell-aoc2023/test/Day20Spec.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day20Spec (spec) where
+
+import Test.Hspec
+
+import Day20
+
+spec :: Spec
+spec = do
+  describe "Part 1" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day20-example-1"
+      doPart1 input `shouldBe` 32000000
+
+--    it "can handle more interesting example" $ do
+--      input <- readFile "inputs/day20-example-2"
+--      doPart1 input `shouldBe` 11687500
+
+--    it "can solve Part 1" $ do
+--      input <- readFile "inputs/day20"
+--      doPart1 input `shouldBe` 0
+
+--  describe "Part 2" $ do
+--    it "can handle given example" $ do
+--      input <- readFile "inputs/day20-example"
+--      doPart2 input `shouldBe` FIXME
+--
+--    it "can solve Part 2" $ do
+--      input <- readFile "inputs/day20"
+--      doPart2 input `shouldBe` 0
